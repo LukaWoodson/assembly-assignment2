72a8cc1579a6647d4b90663e34e4a896556e88db
Luka Woodson <llwoodson@ualr.edu>
2023-10-12 16:36:28 -0500

    Add permArray

--- Assignment 3/assignment3-2.asm
+++ Assignment 3/assignment3-2.asm
@@ -2,17 +2,16 @@
  ### --- Integers and Arrays
 intArray:   .space      40
 permArray:  .space      40
-arraySize:  .word       10
-loopCounter: .word      1
+arraySize:  .word       5
+loopCounter: .word      0
 
 ### --- Instruction Strings
-welcome_message: .asciiz "Welcome! The program will permutate a given array using the provided permutation.\n"
-input_array_message: .asciiz "\nPlease enter the 10 array elements (one at a time)\n"
-permutation_array_message: .asciiz "\nPlease enter the 10 unique permutation elements (one at a time)\n"
+welcome_message: .asciiz "Welcome! The program will calculate an array given array and the provided permutation.\n"
+input_array_message: .asciiz "\nPlease enter the 5 array elements (one at a time)\n"
+permutation_array_message: .asciiz "\nPlease enter the 5 unique permutation elements (one at a time)\n"
 
 ### --- User Prompts
-prompt_p1:     .asciiz     "Please enter an integer "
-prompt_p2:     .asciiz     " value: "
+prompt:     .asciiz     "\nPlease enter an integer value: "
 
 ### --- Results
 input_array_result: .asciiz "\nYour array is: "
@@ -21,20 +20,90 @@ output_array_message: .asciiz "\nThe permuted array is: "
 
 .text
 
+### $t0 = arraySize
+### $t1 = loop counter
+### $t2 = intArray
+### $t3 = permArray
 main:
     ### ----------------------------------- #0 INITIALIZE VARIABLES IN MEMORY
-    ################################################# --------- $t0 = arraySize
     la $t0 arraySize #load arraySize in $t0
-    ################################################# --------- $t1 = loop counter
     la $t1 loopCounter #load loopCounter in $t1
-    ################################################# --------- $t2 = intArray
     la $t2, intArray #load intArray in $t2
+    la $t3, permArray #load permArray in $t3
 
     ### ----------------------------------- #1 DISPLAY WELCOME
     li $v0, 4
     la $a0, welcome_message
     syscall
 
+    j inputMessage
+
+
+### $t0 = arraySize
+### $t1 = loop counter
+### $t2 = intArray
+intLoop:
+    ### ----------------------------------- #1 PROMPT USER FOR INPUT
+    la $a0, prompt
+    li $v0, 4 # 4 - print string
+    syscall
+	
+    ### ----------------------------------- #2 GET INPUT FROM USER AND STORE IN ARRAY
+    li $v0, 5 # 5 - read integer
+    syscall
+    sw $v0, ($t2)
+    
+    ### ----------------------------------- #3 INCREMENT LOOP COUNTER
+    addi $t2, $t2, 4 #move to next slot in intArray
+    addi $t1, $t1, 1
+ 
+    ### ----------------------------------- #4 LOOP UNTIL ALL ELEMENTS ARE ENTERED
+    blt $t1, $t0, intLoop
+
+    ### ----------------------------------- #5 RESET LOOP COUNTER AND LOAD ARRAY INTO $t2
+    li $t1, 0 # reset loop counter
+    la $t2, intArray
+	
+    ### ----------------------------------- #6 DISPLAY INPUT ARRAY RESULT
+    lw $a0, ($t2) # ---------- CURRENTLY DISPLAYING 1ST ELEMENT -----------
+    li $v0, 1  # 1 - print integer
+    syscall
+
+    j permMessage
+
+### $t0 = arraySize
+### $t1 = loop counter
+### $t3 = permArray
+permLoop:
+    ### ----------------------------------- #1 PROMPT USER FOR INPUT
+    la $a0, prompt
+    li $v0, 4
+    syscall
+	
+    ### ----------------------------------- #2 GET INPUT FROM USER AND STORE IN ARRAY
+    li $v0, 5
+    syscall
+    sw $v0, ($t3)
+    
+    ### ----------------------------------- #3 INCREMENT LOOP COUNTER
+    addi $t3, $t3, 4 #move to next slot in intArray
+    addi $t1, $t1, 1
+ 
+    ### ----------------------------------- #4 LOOP UNTIL ALL ELEMENTS ARE ENTERED
+    blt $t1, $t0, permLoop
+
+    ### ----------------------------------- #5 RESET LOOP COUNTER AND LOAD ARRAY INTO $t3
+    li $t1, 0 # reset loop counter
+    la $t3, permArray
+	
+    ### ----------------------------------- #6 DISPLAY INPUT ARRAY RESULT
+    lw $a0, ($t3) # ---------- CURRENTLY DISPLAYING 1ST ELEMENT -----------
+    li $v0, 1
+    syscall
+
+    j exit
+
+inputMessage:
     ### ----------------------------------- #2 DISPLAY INPUT ARRAY INSTRUCTIONS
     li $v0, 4
     la $a0, input_array_message
@@ -43,71 +112,25 @@ main:
     ### ----------------------------------- #3 LOOP TO GET INPUT ARRAY
     lw $t0, arraySize  # Load the value of arraySize into $t0
     lw $t1, loopCounter # Load the value of loopCounter into $t1
-    j intLoop
+    j intLoop	
 
-    ### ----------------------------------- #6 DISPLAY PERMUTATION ARRAY INSTRUCTIONS
+permMessage:
+    ### ----------------------------------- #10 DISPLAY PERMUTATION ARRAY INSTRUCTIONS
+    li $v0, 4
+    la $a0, permutation_array_message
+    syscall
 
-    # 
+    ### ----------------------------------- #11 LOOP TO GET PERM ARRAY
+    j permLoop
 
-    ### ----------------------------------- #7 PROMPT USER FOR INPUT
-    
-    #
+exit:
+    ### ----------------------------------- #12 CALCULATE / PERMUTATE ARRAY
+    # calculate
 
-    ### ----------------------------------- #8 POPULATE ARRAY
- 
-    #
+    ### ----------------------------------- #13 DISPLAY PERMUTED ARRAY
+    # display
 
-    ### ----------------------------------- #9 DISPLAY PERMUTATION ARRAY RESULT
- 
-    #
-
-    ### ----------------------------------- #10 PERMUTATE ARRAY
- 
-    #
-
-    ### ----------------------------------- #11 DISPLAY PERMUTED ARRAY
-     
-    #
-
-    ### ----------------------------------- #12 EXIT PROGRAM
+    ### ----------------------------------- #14 EXIT PROGRAM
     li $v0, 10 # 10 - exit program
     syscall
-
-### ------------------------------------------------------------------------	
-### ------------------------------------------------------------------------	
-### ------------------------------------------------------------------------	
-
-intLoop:
-    ### ----------------------------------- #4 PROMPT USER FOR INPUT
-    la $a0, prompt_p1
-    li $v0, 4 # 4 - print string
-    syscall
-    lw $a0, loopCounter
-    li $v0, 1 # 1 - print integer
-    syscall
-    la $a0, prompt_p2
-    li $v0, 4
-    syscall
-	
-    ### ----------------------------------- #5 GET INPUT FROM USER AND STORE IN ARRAY
-    li $v0, 5 # 5 - read integer
-    syscall
-    sw $v0, ($t2)
-    
-    ### ----------------------------------- #6 INCREMENT LOOP COUNTER
-    addi $t2, $t2, 4 #move to next slot
-    addi $t1, $t1, 1
-	
-    ### ----------------------------------- #7 LOOP UNTIL ALL ELEMENTS ARE ENTERED
-    blt $t1, $t0, intLoop
-
-    ### ----------------------------------- #8 RESET LOOP COUNTER AND LOAD ARRAY INTO $t2
-    li $t2, 0 #reset loop counter
-    la $t2, intArray
-	
-    ### ----------------------------------- #9 DISPLAY INPUT ARRAY RESULT
-    lw $a0, $t2 # ---------- CURRENTLY DISPLAYING 1ST ELEMENT -----------
-    li $v0, 1
-    syscall
-
 
