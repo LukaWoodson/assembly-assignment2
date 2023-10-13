d488a97521db06526b22e009171c29222df18e07
Luka Woodson <llwoodson@ualr.edu>
2023-10-13 18:09:30 -0500

    Add calculations for monsters stats based on players

--- Midterm/midterm.asm
+++ Midterm/midterm.asm
@@ -18,34 +18,54 @@
     playerMinStrength:  .word 5
     playerMaxStrength:  .word 12
     # Monster's min/max strength
-    monsterMinStrength: .word 1  # Adjust these values according to requirements
-    monsterMaxStrength: .word 110  # Adjust these values according to requirements
+    monsterMinStrength: .word 0  # Adjust these values according to requirements
+    monsterMaxStrength: .word 0  # Adjust these values according to requirements
     # Player's min/max health
     playerMinHealth:    .word 25
     playerMaxHealth:    .word 50
     # Monster's min/max health
-    monsterMinHealth:   .word 30  # Adjust these values according to requirements
-    monsterMaxHealth:   .word 55  # Adjust these values according to requirements
+    monsterMinHealth:   .word 0  # Adjust these values according to requirements
+    monsterMaxHealth:   .word 0  # Adjust these values according to requirements
 
 .text
     main:
-       # Generating random values for player's strength and health
-        li $v0, 42     # Load syscall 42 (generate random number)
-        la $a1, playerMinStrength      # Minimum value for random number (player's min strength)
-        la $a2, playerMaxStrength     # Maximum value for random number (player's max strength)
-        syscall        # Execute the syscall
+       ### ------------- GENERATE RANDOM PLAYER STATS -------------
+        li $v0, 42 # Load syscall 42 (generate random number)
+        la $a1, playerMinStrength # Minimum value for random number (player's min strength)
+        la $a2, playerMaxStrength # Maximum value for random number (player's max strength)
+        syscall # Execute the syscall
 
-        sw $v0, characterStats  # Store the random strength value in the array
+        sw $v0, characterStats # Store the random strength value in the array
 
         li $v0, 42     
-        la $a1, playerMinHealth     # Minimum value for random number (player's min health)
-        la $a2, playerMaxHealth     # Maximum value for random number (player's max health)
+        la $a1, playerMinHealth # Minimum value for random number (player's min health)
+        la $a2, playerMaxHealth # Maximum value for random number (player's max health)
         syscall        
 
-        sw $v0, characterStats + 4  # Store the random health value in the array
+        sw $v0, characterStats + 4 # Store the random health value in the array
 
+        ### ------------- GENERATE RANDOM MONSTER 1 STATS -------------
+        # Set the initial values for the monsters' min/max strength and health
+        li $v0, 0  # Load constant 0
+        li $t0, 3  # Load 3 (to represent 1/3 of player's stats)
+        li $t1, 10  # Load 10 (to represent 110% of player's stats)
         
-        
+        # Calculate the values for monsters based on the player's stats
+        # Calculate the values for monsters' strength
+        lw $t2, playerMinStrength  # Load player's min strength
+        lw $t3, playerMaxStrength  # Load player's max strength
+        divu $t2, $t2, $t0  # Calculate 1/3 of player's min strength
+        divu $t3, $t3, $t1  # Calculate 110% of player's max strength
+        sw $t2, monsterMinStrength  # Set monster's min strength
+        sw $t3, monsterMaxStrength  # Set monster's max strength
+
+        # Calculate the values for monsters' health
+        lw $t2, playerMinHealth  # Load player's min health
+        lw $t3, playerMaxHealth  # Load player's max health
+        divu $t2, $t2, $t0  # Calculate 1/3 of player's min health
+        divu $t3, $t3, $t1  # Calculate 110% of player's max health
+        sw $t2, monsterMinHealth  # Set monster's min health
+        sw $t3, monsterMaxHealth  # Set monster's max health
 
 
 
